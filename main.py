d2efd13b63512b1603a30d30f1194da7205f6d4b
Lioueq <71265562+Lioueq@users.noreply.github.com>
2022-03-26 17:24:56 +0300

    -игра переделана под ООП

--- main.py
+++ main.py
@@ -4,48 +4,66 @@ from random import choice
 from vars import pygame_btns, pygame_btns_reversed
 
 
+COLORS = [i for i in range(255)]
+
+
 def color():
     return choice(COLORS), choice(COLORS), choice(COLORS)
 
 
-def render_text():
-    font = pygame.font.SysFont('cera pro', 80)
-    text = font.render(msg, True, (255, 255, 255))
-    text_under = font.render(msg[:count], True, pygame.color.Color('yellow'))
-    screen.blit(text, TEXT_POS)
-    screen.blit(text_under, TEXT_POS)
-    pygame.display.update()
+class Game:
+    def __init__(self):
+        self.SIZE = 800, 600
+        self.TEXT_POS = 240, 240
+        self.FPS = 30
+        self.BACKGROUND = 87, 0, 247
+        self.TEXT_COLOR = 223, 223, 233
+        self.count = 0
+        pygame.init()
+        self.clock = pygame.time.Clock()
+        game_icon = pygame.image.load('icon2.png')
+        pygame.display.set_icon(game_icon)
+        pygame.display.set_caption('Need for nicotine')
+        self.screen = pygame.display.set_mode(self.SIZE)
+        with open('words.txt', encoding='utf-8') as file:
+            unstripped_words_couples = file.readlines()
+            self.words_couples = [i.rstrip('\n') for i in unstripped_words_couples]
+
+    def choice_word_couple(self):
+        self.word_couple = choice(self.words_couples)
+
+    def render_text(self):
+        font = pygame.font.SysFont('cera pro', 80)
+        text = font.render(self.word_couple, True, self.TEXT_COLOR)
+        text_under = font.render(self.word_couple[:self.count], True, (219, 100, 20))
+        self.screen.blit(text, self.TEXT_POS)
+        self.screen.blit(text_under, self.TEXT_POS)
+        pygame.display.update()
+
+    def run(self):
+        self.choice_word_couple()
+        run = True
+        while run:
+            self.clock.tick(self.FPS)
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    run = False
+                if event.type == pygame.MOUSEMOTION:
+                    # print(pygame.mouse.get_pos())
+                    pass
+                if event.type == pygame.KEYDOWN:
+                    need_button = pygame.key.key_code(self.word_couple[self.count])
+                    print(need_button)
+                    print(event.key)
+                    keys = pygame.key.get_pressed()
+                    if keys[need_button]:
+                        self.count += 1
+            if self.count == len(self.word_couple):
+                self.count = 0
+                self.choice_word_couple()
+            self.screen.fill(self.BACKGROUND)
+            self.render_text()
+            pygame.display.flip()
 
 
-SIZE = 800, 600
-TEXT_POS = 240, 240
-FPS = 30
-COLORS = [i for i in range(255)]
-BLACK = 0, 0, 0
-
-count = 0
-msg = 'DETEST ME'
-
-pygame.init()
-clock = pygame.time.Clock()
-game_icon = pygame.image.load('icon.png')
-pygame.display.set_icon(game_icon)
-pygame.display.set_caption('Need for nicotine')
-screen = pygame.display.set_mode(SIZE)
-run = True
-while run:
-    clock.tick(FPS)
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            run = False
-        if event.type == pygame.MOUSEMOTION:
-            # print(pygame.mouse.get_pos())
-            pass
-        if event.type == pygame.KEYDOWN:
-            need_button = pygame.key.key_code(msg[count])
-            keys = pygame.key.get_pressed()
-            if keys[need_button]:
-                count += 1
-    screen.fill(BLACK)
-    render_text()
-    pygame.display.flip()
+Game().run()
--- vars.py
+++ vars.py
@@ -1,3 +1,4 @@
+
 pygame_btns = {'K_a': 'a', 'K_b': 'b', 'K_c': 'c', 'K_d': 'd', 'K_e': 'e', 'K_f': 'f', 'K_g': 'g', 'K_h': 'h',
                'K_i': 'i', 'K_j': 'j', 'K_k': 'k', 'K_l': 'l', 'K_m': 'm', 'K_n': 'n', 'K_o': 'o', 'K_p': 'p',
                'K_q': 'q', 'K_r': 'r', 'K_s': 's', 'K_t': 't', 'K_u': 'u', 'K_v': 'v', 'K_w': 'w', 'K_x': 'x',
@@ -7,4 +8,3 @@ pygame_btns_reversed = {'a': 'K_a', 'b': 'K_b', 'c': 'K_c', 'd': 'K_d', 'e': 'K_
                         'h': 'K_h', 'i': 'K_i', 'j': 'K_j', 'k': 'K_k', 'l': 'K_l', 'm': 'K_m', 'n': 'K_n',
                         'o': 'K_o', 'p': 'K_p', 'q': 'K_q', 'r': 'K_r', 's': 'K_s', 't': 'K_t', 'u': 'K_u',
                         'v': 'K_v', 'w': 'K_w', 'x': 'K_x', 'y': 'K_y', 'z': 'K_z'}
-
