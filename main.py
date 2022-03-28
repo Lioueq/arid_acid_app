f7da7439ec041c608da4ec514da2f41c12ffef0f
Lioueq <71265562+Lioueq@users.noreply.github.com>
2022-03-28 16:46:49 +0300

    added timer added error_counter upgraded subline_render_function

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import pygame
 import pygame_gui
+import time
 from random import choice
-from vars import from_russian_letters_id_to_english_letters_id_capslock_off
+from vars import from_russian_letters_id_to_english_letters_id_capslock_off, cera_pro_book_size
 
 
 COLORS = [i for i in range(255)]
@@ -18,7 +19,8 @@ class Game:
         self.FPS = 30
         self.BACKGROUND = pygame.Color('#08592e')
         self.TEXT_COLOR = pygame.Color('#b383dd')
-        self.CAPS_LOCK = False
+        self.error = False
+        self.shift = 0
         self.count = 0
         self.is_running = True
         pygame.init()
@@ -35,12 +37,30 @@ class Game:
         self.word_couple = choice(self.words_couples)
 
     def render_text(self):
-        font = pygame.font.SysFont('cera pro', 65)
-        text = font.render(self.word_couple, True, self.TEXT_COLOR)
-        text_under = font.render(self.word_couple[:self.count], True, pygame.Color(250, 167, 0))
+        # text to write
+        FONT = pygame.font.Font('data/cera_pro_fonts_forms/CeraPro-Regular.ttf', 60)
+        text = FONT.render(self.word_couple, True, self.TEXT_COLOR)
+        text_under = FONT.render(self.word_couple[:self.count], True, pygame.Color(250, 167, 0))
+        # text with error number
+        text_error = FONT.render(f'Количество ошибок: {self.error_count}', True, pygame.Color(250, 167, 0))
         self.screen.blit(text, self.TEXT_POS)
         self.screen.blit(text_under, self.TEXT_POS)
+        self.screen.blit(text_error, (self.TEXT_POS[0], self.TEXT_POS[1] + 100))
         pygame.display.update()
+
+    def render_subline(self):
+        subline = pygame.Surface((cera_pro_book_size[self.word_couple[self.count]], 4))
+        if self.error:
+            subline.fill(pygame.Color('red'))
+        else:
+            subline.fill(pygame.Color(250, 167, 0))
+        self.screen.blit(subline, (self.TEXT_POS[0] + self.shift + 5 * self.count,
+                                   self.TEXT_POS[1] + 65))
+
+    def render_time_text(self, time_delta):
+        FONT = pygame.font.Font('data/cera_pro_fonts_forms/CeraPro-Regular.ttf', 60)
+        time_text = FONT.render(f'Осталось времени: {int(60 - time_delta)}', True, pygame.Color(250, 167, 0))
+        self.screen.blit(time_text, (140, 50))
 
     def main_window_scene(self):
         manager = pygame_gui.UIManager(self.SIZE, 'data/theme.json')
@@ -64,15 +84,24 @@ class Game:
             pygame.display.update()
 
     def write_word_scene(self):
+        start_time = time.time()
         self.choice_word_couple()
+        self.error_count = 0
+        click_count = 0
         while self.is_running:
+            time_delta = time.time() - start_time
             self.clock.tick(self.FPS)
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     self.is_running = False
-                if event.type == pygame.MOUSEMOTION:
-                    print(pygame.mouse.get_pos())
-                    pass
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    click_count += 1
+                    if click_count == 1:
+                        x1 = pygame.mouse.get_pos()[0]
+                    elif click_count == 2:
+                        x2 = pygame.mouse.get_pos()[0]
+                        print(x2 - x1)
+                        click_count = 0
                 # KEYBOARD EVENTS
                 if event.type == pygame.KEYDOWN:
                     need_button = pygame.key.key_code(self.word_couple[self.count])  # NEED_BUTTON_ID
@@ -82,14 +111,26 @@ class Game:
                         need_button = from_russian_letters_id_to_english_letters_id_capslock_off[need_button]
                     keys = pygame.key.get_pressed()
                     #  ПРОВЕРКА НА НАЖАТИЕ ID ТОЙ БУКВЫ, ЧТО УКАЗАНА В ТЕКСТЕ
-                    if keys[need_button] or keys[pygame.K_RALT]:
+                    if keys[need_button]:
+                        self.shift += cera_pro_book_size[self.word_couple[self.count]]
                         self.count += 1
+                        self.error = False
+                    elif keys[8]:
+                        self.shift -= cera_pro_book_size[self.word_couple[self.count - 1]]
+                        self.count -= 1
+                    elif not keys[need_button] and (event.key != pygame.K_LSHIFT and event.key != pygame.K_LALT):
+                        self.error = True
+                        self.error_count += 1
             #  RESET
             if self.count == len(self.word_couple):
                 self.count = 0
                 self.choice_word_couple()
+                self.error_count = 0
+                self.shift = 0
             self.screen.fill(self.BACKGROUND)
             self.render_text()
+            self.render_subline()
+            self.render_time_text(time_delta)
             pygame.display.flip()
 
 
